2997276112a0310dbf550ba416fd2b7427d027b4
Ethan Smith <ethan@ethanhs.me>
2023-05-20 01:49:19 -0400

    Add async

--- src/async_gotify.rs
+++ src/async_gotify.rs
@@ -1,53 +1,29 @@
 use std::collections::HashMap;
-use std::fs::File;
 use std::str::FromStr;
 
-use reqwest::blocking::Client as SyncClient;
-use reqwest::Method;
-
 use anyhow::Result;
+use reqwest::Method;
 use serde::de::DeserializeOwned;
 
-mod async_gotify;
-mod response_types;
-pub use crate::async_gotify::*;
-pub use crate::response_types::*;
+use async_trait::async_trait;
+use reqwest::Client as AsyncClient;
 
-pub struct Gotify<'a> {
-    base_url: &'a str,
-    app_token: Option<&'a str>,
-    client_token: Option<&'a str>,
-}
+use crate::response_types::*;
+use crate::Gotify;
 
-impl<'a> Gotify<'a> {
-    pub fn new(base_url: &'a str, app_token: &'a str, client_token: &'a str) -> Self {
-        Self {
-            base_url: base_url.trim_end_matches("/"),
-            app_token: Some(app_token),
-            client_token: Some(client_token),
-        }
-    }
-    pub fn config(mut self, base_url: &'a str, app_token: &'a str, client_token: &'a str) -> Self {
-        self.base_url = base_url.trim_end_matches("/");
-        self.app_token = Some(app_token);
-        self.client_token = Some(client_token);
-        self
-    }
-}
-
-pub struct SyncGotify<'a> {
+pub struct AsyncGotify<'a> {
     gotify: Gotify<'a>,
-    client: SyncClient,
+    client: AsyncClient,
 }
 
-impl<'a> SyncGotify<'a> {
+impl<'a> AsyncGotify<'a> {
     pub fn new(base_url: &'a str, app_token: &'a str, client_token: &'a str) -> Self {
         let gotify = Gotify::new(base_url, app_token, client_token);
-        let client = SyncClient::new();
+        let client = AsyncClient::new();
         Self { gotify, client }
     }
     pub fn from(gotify: Gotify<'a>) -> Self {
-        let client = SyncClient::new();
+        let client = AsyncClient::new();
         Self { gotify, client }
     }
 
@@ -62,13 +38,111 @@ impl<'a> SyncGotify<'a> {
             self.gotify.app_token
         }
     }
+}
 
-    fn do_request<T: DeserializeOwned>(
+#[async_trait]
+trait AsyncGotifyImpl {
+    async fn do_request<T: DeserializeOwned>(
         &self,
         method: &str,
         endpoint_url: &str,
         data: Option<HashMap<String, Option<String>>>,
-        file: Option<File>,
+        file: Option<tokio::fs::File>,
+        auth_mode: Option<&str>,
+    ) -> Result<T>;
+
+    async fn applications(&self) -> Result<Vec<Application>>;
+
+    async fn create_application(&self, name: String, description: String) -> Result<Application>;
+
+    async fn update_application(
+        &self,
+        id: i32,
+        name: String,
+        description: Option<String>,
+    ) -> Result<Application>;
+
+    async fn delete_application(&self, id: i32) -> Result<()>;
+
+    async fn upload_application_image(
+        &self,
+        id: i32,
+        image: tokio::fs::File,
+    ) -> Result<Application>;
+
+    async fn get_messages(
+        &self,
+        app_id: Option<i32>,
+        limit: Option<i32>,
+        since: Option<i32>,
+    ) -> Result<PagedMessages>;
+
+    async fn create_message(
+        &self,
+        message: String,
+        priority: Option<i32>,
+        title: Option<String>,
+        // TODO: extras
+    ) -> Result<Message>;
+
+    async fn delete_messages(&self, app_id: Option<i32>) -> Result<()>;
+
+    async fn delete_message(&self, msg_id: i32) -> Result<()>;
+
+    async fn get_clients(&self) -> Result<Vec<Client>>;
+
+    async fn create_client(&self, name: String) -> Result<Client>;
+
+    async fn update_client(&self, id: i32, name: String) -> Result<Client>;
+
+    async fn delete_client(&self, id: i32) -> Result<()>;
+
+    async fn get_current_user(&self) -> Result<User>;
+
+    async fn set_password(&self, passwd: String) -> Result<()>;
+
+    async fn get_users(&self) -> Result<Vec<User>>;
+
+    async fn create_user(&self, name: String, passwd: String, admin: Option<bool>) -> Result<User>;
+
+    async fn get_user(&self, id: i32) -> Result<User>;
+
+    async fn update_user(
+        &self,
+        id: i32,
+        name: Option<String>,
+        passwd: Option<String>,
+        admin: Option<bool>,
+    ) -> Result<User>;
+
+    async fn delete_user(&self, id: i32) -> Result<()>;
+
+    async fn get_health(&self) -> Result<Health>;
+
+    async fn get_plugins(&self) -> Result<Vec<PluginConf>>;
+
+    async fn get_plugin_config(&self, id: i32) -> Result<PluginConf>;
+
+    /// TODO(ethanhs): Figure out what this looks like
+    /* async fn update_plugin_config(&self, id: i32); */
+
+    async fn disable_plugin(&self, id: i32) -> Result<()>;
+
+    async fn get_plugin_display(&self, id: i32) -> Result<String>;
+
+    async fn enable_plugin(&self, id: i32) -> Result<()>;
+
+    async fn get_version(&self) -> Result<VersionInfo>;
+}
+
+#[async_trait]
+impl<'a> AsyncGotifyImpl for AsyncGotify<'a> {
+    async fn do_request<T: DeserializeOwned>(
+        &self,
+        method: &str,
+        endpoint_url: &str,
+        data: Option<HashMap<String, Option<String>>>,
+        file: Option<tokio::fs::File>,
         auth_mode: Option<&str>,
     ) -> Result<T> {
         let method = Method::from_str(&method)?;
@@ -91,22 +165,24 @@ impl<'a> SyncGotify<'a> {
         }
         let token = self.get_token(auth_mode).expect("missing token");
         request = request.header("X-Gotify-Key", token);
-        let response = request.send()?;
-        Ok(response.json::<T>()?)
+        let response = request.send().await?;
+        Ok(response.json::<T>().await?)
     }
 
-    pub fn applications(&self) -> Result<Vec<Application>> {
+    async fn applications(&self) -> Result<Vec<Application>> {
         self.do_request("get", "/applications", None, None, None)
+            .await
     }
 
-    pub fn create_application(&self, name: String, description: String) -> Result<Application> {
+    async fn create_application(&self, name: String, description: String) -> Result<Application> {
         let mut data = HashMap::new();
         data.insert("name".to_owned(), Some(name));
         data.insert("description".to_owned(), Some(description));
         self.do_request("post", "/application", Some(data), None, None)
+            .await
     }
 
-    pub fn update_application(
+    async fn update_application(
         &self,
         id: i32,
         name: String,
@@ -116,13 +192,19 @@ impl<'a> SyncGotify<'a> {
         data.insert("name".to_string(), Some(name));
         data.insert("description".to_string(), description);
         self.do_request("put", &format!("/application/{id}"), Some(data), None, None)
+            .await
     }
 
-    pub fn delete_application(&self, id: i32) -> Result<()> {
+    async fn delete_application(&self, id: i32) -> Result<()> {
         self.do_request("delete", &format!("/application/{id}"), None, None, None)
+            .await
     }
 
-    pub fn upload_application_image(&self, id: i32, image: File) -> Result<Application> {
+    async fn upload_application_image(
+        &self,
+        id: i32,
+        image: tokio::fs::File,
+    ) -> Result<Application> {
         self.do_request(
             "post",
             &format!("/application/{id}/image"),
@@ -130,9 +212,10 @@ impl<'a> SyncGotify<'a> {
             Some(image),
             None,
         )
+        .await
     }
 
-    pub fn get_messages(
+    async fn get_messages(
         &self,
         app_id: Option<i32>,
         limit: Option<i32>,
@@ -149,12 +232,14 @@ impl<'a> SyncGotify<'a> {
                 None,
                 None,
             )
+            .await
         } else {
             self.do_request("get", "/message", Some(data), None, None)
+                .await
         }
     }
 
-    pub fn create_message(
+    async fn create_message(
         &self,
         message: String,
         priority: Option<i32>,
@@ -166,9 +251,10 @@ impl<'a> SyncGotify<'a> {
         data.insert("priority".to_string(), priority.map(|i| i.to_string()));
         data.insert("title".to_string(), title);
         self.do_request("post", "/message", Some(data), None, Some("app"))
+            .await
     }
 
-    pub fn delete_messages(&self, app_id: Option<i32>) -> Result<()> {
+    async fn delete_messages(&self, app_id: Option<i32>) -> Result<()> {
         if let Some(id) = app_id {
             self.do_request(
                 "delete",
@@ -177,50 +263,58 @@ impl<'a> SyncGotify<'a> {
                 None,
                 None,
             )
+            .await
         } else {
             self.do_request("delete", &format!("/message"), None, None, None)
+                .await
         }
     }
 
-    pub fn delete_message(&self, msg_id: i32) -> Result<()> {
+    async fn delete_message(&self, msg_id: i32) -> Result<()> {
         self.do_request("delete", &format!("/message/{msg_id}"), None, None, None)
+            .await
     }
 
-    pub fn get_clients(&self) -> Result<Vec<Client>> {
-        self.do_request("get", "/client", None, None, None)
+    async fn get_clients(&self) -> Result<Vec<Client>> {
+        self.do_request("get", "/client", None, None, None).await
     }
 
-    pub fn create_client(&self, name: String) -> Result<Client> {
+    async fn create_client(&self, name: String) -> Result<Client> {
         let mut data = HashMap::new();
         data.insert("name".to_string(), Some(name));
         self.do_request("post", "/client", Some(data), None, None)
+            .await
     }
 
-    pub fn update_client(&self, id: i32, name: String) -> Result<Client> {
+    async fn update_client(&self, id: i32, name: String) -> Result<Client> {
         let mut data = HashMap::new();
         data.insert("name".to_string(), Some(name));
         self.do_request("put", &format!("/client/{id}"), Some(data), None, None)
+            .await
     }
 
-    pub fn delete_client(&self, id: i32) -> Result<()> {
+    async fn delete_client(&self, id: i32) -> Result<()> {
         self.do_request("delete", &format!("/client/{id}"), None, None, None)
+            .await
     }
 
-    pub fn get_current_user(&self) -> Result<User> {
+    async fn get_current_user(&self) -> Result<User> {
         self.do_request("get", "/current/user", None, None, None)
+            .await
     }
 
-    pub fn set_password(&self, passwd: String) -> Result<()> {
+    async fn set_password(&self, passwd: String) -> Result<()> {
         let mut data = HashMap::new();
         data.insert("pass".to_string(), Some(passwd));
         self.do_request("get", "/current/user/password", Some(data), None, None)
+            .await
     }
 
-    pub fn get_users(&self) -> Result<Vec<User>> {
-        self.do_request("get", "/user", None, None, None)
+    async fn get_users(&self) -> Result<Vec<User>> {
+        self.do_request("get", "/user", None, None, None).await
     }
 
-    pub fn create_user(&self, name: String, passwd: String, admin: Option<bool>) -> Result<User> {
+    async fn create_user(&self, name: String, passwd: String, admin: Option<bool>) -> Result<User> {
         let mut data = HashMap::new();
         data.insert("pass".to_string(), Some(passwd));
         data.insert("name".to_string(), Some(name));
@@ -233,13 +327,15 @@ impl<'a> SyncGotify<'a> {
             }),
         );
         self.do_request("post", "/user", Some(data), None, None)
+            .await
     }
 
-    pub fn get_user(&self, id: i32) -> Result<User> {
+    async fn get_user(&self, id: i32) -> Result<User> {
         self.do_request("get", &format!("/user/{id}"), None, None, None)
+            .await
     }
 
-    pub fn update_user(
+    async fn update_user(
         &self,
         id: i32,
         name: Option<String>,
@@ -258,42 +354,48 @@ impl<'a> SyncGotify<'a> {
             }),
         );
         self.do_request("put", &format!("/user/{id}"), Some(data), None, None)
+            .await
     }
 
-    pub fn delete_user(&self, id: i32) -> Result<()> {
+    async fn delete_user(&self, id: i32) -> Result<()> {
         self.do_request("delete", &format!("/user/{id}"), None, None, None)
+            .await
     }
 
-    pub fn get_health(&self) -> Result<Health> {
-        self.do_request("get", "/health", None, None, None)
+    async fn get_health(&self) -> Result<Health> {
+        self.do_request("get", "/health", None, None, None).await
     }
 
-    pub fn get_plugins(&self) -> Result<Vec<PluginConf>> {
-        self.do_request("get", "/plugins", None, None, None)
+    async fn get_plugins(&self) -> Result<Vec<PluginConf>> {
+        self.do_request("get", "/plugins", None, None, None).await
     }
 
-    pub fn get_plugin_config(&self, id: i32) -> Result<PluginConf> {
+    async fn get_plugin_config(&self, id: i32) -> Result<PluginConf> {
         self.do_request("get", &format!("/plugins/{id}/config"), None, None, None)
+            .await
     }
 
     /// TODO(ethanhs): Figure out what this looks like
-    /* pub fn update_plugin_config(&self, id: i32) {
+    /* async fn update_plugin_config(&self, id: i32) {
         unimplemented!()
     } */
 
-    pub fn disable_plugin(&self, id: i32) -> Result<()> {
+    async fn disable_plugin(&self, id: i32) -> Result<()> {
         self.do_request("post", &format!("/plugins/{id}/disable"), None, None, None)
+            .await
     }
 
-    pub fn get_plugin_display(&self, id: i32) -> Result<String> {
+    async fn get_plugin_display(&self, id: i32) -> Result<String> {
         self.do_request("get", &format!("/plugins/{id}/display"), None, None, None)
+            .await
     }
 
-    pub fn enable_plugin(&self, id: i32) -> Result<()> {
+    async fn enable_plugin(&self, id: i32) -> Result<()> {
         self.do_request("post", &format!("/plugins/{id}/enable"), None, None, None)
+            .await
     }
 
-    pub fn get_version(&self) -> Result<VersionInfo> {
-        self.do_request("get", "/version", None, None, None)
+    async fn get_version(&self) -> Result<VersionInfo> {
+        self.do_request("get", "/version", None, None, None).await
     }
 }
